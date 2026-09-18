76855692cbb9a8bc80338c4afe8582f78cf4d0e8
SofiaBritps <sofiibritos01@gmail.com>
2026-09-18 11:16:51 -0300

    (feat): routes privacy policy an terms config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,12 @@ import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy';
+import { TermsComponent } from './terms/terms';
+import { SecurityPolicyComponent } from './security-policy/security-policy';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'politica-de-privacidad', component: PrivacyPolicyComponent },
+  { path: 'terminos-y-condiciones', component: TermsComponent },
+  { path: 'politica-de-seguridad', component: SecurityPolicyComponent },
 ];
---
+++ src/app/security-policy/security-policy.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-security-policy',
+  templateUrl: './security-policy.html',
+  styleUrl: './security-policy.css',
+})
+export class SecurityPolicyComponent {}
