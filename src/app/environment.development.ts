62a37fab4a37790fa99c7a12b0f9d7911067dcbd
Ana Di Marco <anabellegatti@gmail.com>
2026-09-18 10:01:01 -0300

    Update environment.development.ts

--- src/app/environment.development.ts
+++ src/app/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  googleSheetsUrl: 'https://script.google.com/macros/library/d/1SybL1vaARy2usilVf0jW1wgQoAljxTYy0QkoJF7u4fsG4yVCj3zKGWj5/1',
+  googleSheetsUrl: 'https://script.google.com/macros/s/AKfycbxYH6gMnqpYXIhcZecuKbJDwwVgWPToUmJipZ8Qx53HW2UfhKjAO2NFBSFmetemwn3ESQ/exec',
 };
