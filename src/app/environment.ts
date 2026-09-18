29e6456abfb44b3dba80ad4a83f9c51798a59311
Ana Di Marco <anabellegatti@gmail.com>
2026-09-18 11:42:51 -0300

    Delete src/app/environment.ts

--- src/app/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  googleSheetsUrl: 'https://script.google.com/macros/s/AKfycbxYH6gMnqpYXIhcZecuKbJDwwVgWPToUmJipZ8Qx53HW2UfhKjAO2NFBSFmetemwn3ESQ/exec',
-};
